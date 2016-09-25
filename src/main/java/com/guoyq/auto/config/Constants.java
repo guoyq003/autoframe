5ababfcf8e4e84d9fe2662dfe92142c80e8dd217
guoyq <65698173@qq.com>
2016-09-25 22:05:08 +0800

    init

---
+++ src/main/java/com/guoyq/auto/config/Constants.java
@@ -0,0 +1,33 @@
+package com.guoyq.auto.config;
+
+public class Constants {
+
+    public static String IE = "IE";
+    public static String CHROME = "Chrome";
+    public static String FireFox = "firefox";
+
+    public static String IEName = "iexplore.exe";
+    public static String ChromeName = "chrome.exe";
+    public static String FirefoxName = "firefox.exe";
+
+    public static String IEDriverName = "IEDriverServer.exe";
+    public static String IEDriverPath ="c:\\autoSVN\\driver\\IEDriverServer.exe";
+    public static String IEProperty = "webdriver.ie.driver";
+    public static int TimeOut = 0;
+
+    public static String FirefoxProperty = "webdriver.firefox.bin";
+    public static String FirefoxPath = "D:\\worksoft\\Mozilla Firefox\\firefox.exe";
+
+    public static String ChromeDriverName = "chromedriver.exe";
+    public static String ChromeProperty = "webdriver.chrome.driver";
+    public static String ChromeDriverPath = "c:\\autoSVN\\driver\\chromedriver.exe";
+
+    public static String LogXML = "./config/log4j.xml";
+    public static String LogProperty = "./config/log4j.properties";
+    public static String config = "./config/config.properties";
+    public static String Out_Dir = "./report";
+    public static String DUP_MARK = "@";
+
+    public static String Semicolon = ";";
+    public static String Comma = ",";
+}
---
+++ src/main/java/com/guoyq/auto/managecase/RunTest.java
@@ -0,0 +1,7 @@
+package com.guoyq.auto.managecase;
+
+/**
+ * Created by guoyq on 2016/9/24.
+ */
+public class RunTest {
+}
